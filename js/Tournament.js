c29a83f51b6d685a4df860d811370da5357a7076
Kenzhie16 <kelaco16@gmail.com>
2022-12-05 21:14:03 +0800

    adding files

---
+++ js/Tournament.js
@@ -0,0 +1,37 @@
+function openConsole(evt, console) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(console).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+  window.addEventListener('load', function () {
+    var anime = document.querySelectorAll('.animeX');
+
+    for (var s = 0; s < anime.length; s++) {
+        var windowheight = window.innerHeight;
+        var animetop = anime[s].getBoundingClientRect().top;
+        var animepoint = 150;
+
+        if (animetop < windowheight - animepoint) {
+            anime[s].classList.add('active');
+        }
+        else {
+            anime[s].classList.remove('active');
+        }
+    }
+})
